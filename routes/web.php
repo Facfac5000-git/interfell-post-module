bb9aa687f46dca9a8aca95e14f5352936a6020dd
Facfac5000-git <facfac4000@hotmail.com>
2021-03-26 13:51:43 -0300

    Routes created

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -24,6 +17,9 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+Route::get('dashboard', [PageController::class, 'dashboard'])
+    ->middleware('auth:sanctum')
+    ->name('dashboard');
+
+Route::resource('posts', PostController::class)
+    ->middleware('auth:sanctum');
